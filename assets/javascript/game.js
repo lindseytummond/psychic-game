3c5b216e905275be521d010b52477bb24247dae5
lindsey tummond <lindseytummond@gmail.com>
2020-02-13 20:04:05 -0700

    initial files for project work

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+//Use the src attribute of the script tag to link to this file, 
+//rather than embedding the code directly in your HTML document.
+//The src attribute specifies the URL of the media file to play. 
+//This attribute is required when <source> is used in <audio> and <video>.
