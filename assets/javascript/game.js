7f68b8e083e0a3d4ae81994009d0de0a2961897b
lindsey tummond <lindseytummond@gmail.com>
2020-02-15 22:50:26 -0700

    button colors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -54,22 +54,22 @@ function compareGuess(){
             document.getElementById('inputBox').value = "";
         } else {
             document.getElementById('guess-text').innerHTML = 'You Win, Your Psychic Abilities guessed the Letter in ' + attempts + ' attempts';
-            document.getElementById('container').style.backgroundColor = 'green';
+            document.getElementById('container').style.backgroundColor = '#2DBC42';
             gameEnded();
         }
 
     } else {
         if(userGuess < computerGuess){
             document.getElementById('guess-text').innerHTML = 'You Lose, You must Not be Psychic. The correct Letter is ' + computerGuess;
-            document.getElementById('container').style.backgroundColor = '#e82c4e';
+            document.getElementById('container').style.backgroundColor = '#DD1527';
             gameEnded();
         } else if (userGuess < computerGuess){
             document.getElementById('guess-text').innerHTML = 'You Lose, You must Not be Psychic. The correct Letter is ' + computerGuess;
-            document.getElementById('container').style.backgroundColor = '#e82c4e';
+            document.getElementById('container').style.backgroundColor = '#DD1527';
             gameEnded();
         } else {
             document.getElementById('guess-text').innerHTML = 'You Win, Your Psychic Abilities guessed the Letter in ' + attempts + ' attempts';
-            document.getElementById('container').style.backgroundColor = 'green';
+            document.getElementById('container').style.backgroundColor = '#2DBC42';
             gameEnded();
         }
     }
